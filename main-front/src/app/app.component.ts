796e494d44aea0ea5480eef36e84d165533dccf0
Adam Taifouri <iiadamx@gmail.com>
2024-06-02 14:57:31 -0400

    added conditional logic for element animations based on viewport

--- main-front/src/app/app.component.ts
+++ main-front/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import {Component, HostBinding} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { InViewportModule } from 'ng-in-viewport';
+import { CommonModule } from '@angular/common';
 import { BioComponent } from './bio/bio.component';
 import {
   trigger,
@@ -14,7 +16,7 @@ import {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, BioComponent],
+  imports: [RouterOutlet, CommonModule, HeaderComponent, BioComponent, InViewportModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- main-front/src/app/bio/bio.component.ts
+++ main-front/src/app/bio/bio.component.ts
@@ -1,4 +1,5 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, NgModule, ChangeDetectorRef } from '@angular/core';
+import { InViewportModule } from 'ng-in-viewport';
 import {
   trigger,
   state,
@@ -7,41 +8,39 @@ import {
   transition,
   // ...
 } from '@angular/animations';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-bio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, InViewportModule],
   templateUrl: './bio.component.html',
   styleUrl: './bio.component.css',
   animations: [
     trigger('pullIn', [
-      state('right', style({ opacity: '0%', transform: 'translateX(100vw)' })),
-      state('left', style({ opacity: '0%', transform: 'translateX(-100vw)' })),
+      state('rightPull', style({ opacity: '0%', transform: 'translateX(100px)' })),
+      state('leftPull', style({ opacity: '0%', transform: 'translateX(-100px)' })),
       state('leftResting', style({ opacity: '100%', transform: 'translateX(0px)' })),
       state('rightResting', style({ opacity: '100%', transform: 'translateX(0px)' })),
 
-      transition('right <=> rightResting', [animate('2s')]),
-      transition('left <=> leftResting', [animate('2s')]),
+      transition('rightPull <=> rightResting', [animate('1s')]),
+      transition('leftPull <=> leftResting', [animate('1s')]),
     ]),
-    trigger('bounce', [
-      state('bounceDown', style({ transform: 'translateY(0px)' })),
-      state('bounceUp', style({ transform: 'translateY(-10px)' })),
-      
-      transition('bounceDown <=> bounceUp', [animate('2s')]),
-   ]),
-  ]
-})
+   ]})
+    
 
 export class BioComponent {
-  onBioSection = true;
-  isBouncing = false;
+  onBioSection = false;
+  constructor(private cdr: ChangeDetectorRef) {}
 
   toggleOnBioSection(){
     this.onBioSection = !this.onBioSection;
   }
-
-  toggleBounce(){
-    this.isBouncing = !this.isBouncing;
+    
+  public onIntersection({ target, visible }: { target: Element; visible: boolean }): void {
+      this.onBioSection = visible;
+      //modifying binding in onIntersection() doesn't trigger detectChanges so do so manually
+      this.cdr.detectChanges();
   }
+
 }
